ee0acda36b1ace6f09c966577a6f72b9f9a86f10
sundaram6044 <sundarammishra6044@gmail.com>
2026-08-20 11:10:03 +0530

    Delete init.js

--- init.js
+++
@@ -1,9 +0,0 @@
-/* ===================================================================
-   init.js
-   Starts the app. Must load LAST, after every other file has defined
-   its functions. Add any new "run once on startup" calls here.
-   =================================================================== */
-
-refreshPenUI();
-resize();
-renderPageStrip();
